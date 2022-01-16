30d194b7048a6910b517ce1de3a33c58e3471c28
Scott Will <sdwill94@gmail.com>
2022-01-16 12:50:30 -0500

    Add julia language server

--- vim/.vim/plugins/nvim-lspconfig.lua
+++ vim/.vim/plugins/nvim-lspconfig.lua
@@ -46,7 +46,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- local servers = { 'julials', 'sumneko_lua', 'pylsp'}
-local servers = { 'sumneko_lua', 'pylsp' }
+local servers = { 'sumneko_lua', 'pylsp' , 'julials'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
