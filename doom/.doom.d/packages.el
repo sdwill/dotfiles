d66af8734884cdb6b21bb477f993d0c361857341
Scott D Will <scott.d.will@nasa.gov>
2023-07-13 14:06:49 -0400

    Doom: add some theme packages

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -9,6 +9,12 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! org-super-agenda)
+(package! nano-theme)
+(package! autothemer)
+(package! catppuccin-theme)
+(package! solarized-theme)
+;; (package! everforest :recipe (:repo "https://git.sr.ht/~theorytoe/everforest-theme"))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
